9f5a1e7a2d80ea6a3ec23d427763f35284897f94
feetoffire <jbh14b@acu.edu>
2017-10-25 20:53:29 -0500

    Add files via upload
    added buidling derived classes

--- adventure.h
+++ adventure.h
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <cmath>
 
+class campusCenter : public Building
+{
+	private:
+		//
+	public:
+		void eatAtBean();
+};
+
+class MBB : public Building
+{
+	private:
+		//
+	public:
+		void goToClass();
+		void isOnTime();
+};
+
+class barretHall : public Building
+{
+	private:
+		//
+	public:
+		void sleep();
+};
