8cc95c7810aa184b56664244a649b6bb9b4edea4
Gideon Luck <gbl12a@acu.edu>
2017-11-13 23:21:09 -0600

    Add files via upload

--- adventure.h
+++ adventure.h
@@ -4,13 +4,11 @@
 #include <string>
 using namespace std;
 	
-bool gamecomplete = false; 
-   
 
 class World
 {
     private:
-		
+		bool exists;
     public:
     	//world constructor could call all our building constructors 
 	    World();
@@ -23,13 +21,13 @@ class Player
 {
     private:
 		bool exists;
-        
     public:
     	class invalidMove{};
 
 	    Player(){ exists = true; }
-        ~Player(){}
-    	void move(string userinput);
+        ~Player(){ exists = false; }
+        
+    	static void move(string userinput);
     	
 };
 class Building
@@ -41,181 +39,187 @@ class Building
         int* hasItems;
     public:
     	// 
-        Building(){}
-        virtual ~Building(){}
+        Building(){location = 0; }
+        ~Building(){}
         
-        virtual void enter();
-        virtual void exit();
-        virtual void search(); 
-
+        virtual void enter() = 0;
+        virtual void exit()= 0;
+        virtual void search() = 0; 
+        virtual void doThis(string userInput) = 0;
 };
 
 class Library : public Building
 {
     private:
-
+	    double location;
+        bool hasBeenVisited;
+        bool isLocked;
+        int* hasItems;
     public:
+    	class tryagain{};
         Library();
         ~Library();
-        getCoffee();
+        
+        void getCoffee();
            
         void enter();
         void exit();
-        void search(); 
+        void search();
+        void doThis(string userInput);
 
 };
 
-class Moody : public Building
-{
-    private:
-
-    public:
-        Moody();
-        ~Moody();      
-        void worship();
-        
-        void enter();
-        void exit();
-        void search(); 
-};
-
-class RecCenter : public Building
-{
-    private:
-
-    public:
-        RecCenter();
-        ~RecCenter();
-        void workOut();
-                   
-        void enter();
-        void exit();
-        void search(); 
-};
-class CampusCenter : public Building
-{
-	private:
-		//
-    public:
-        CampusCenter();
-        ~CampusCenter();
-		void eatAtBean();
-		           
-        void enter();
-        void exit();
-        void search(); 
-};
-
-class MBB : public Building
-{
-	private:
-		//
-    public:
-        MBB();
-        ~MBB();
-		void goToClass();
-		void isOnTime();
-		           
-        void enter();
-        void exit();
-        void search(); 
-};
-
-class BarretHall : public Building
-{
-	private:
-		//
-    public:
-        BarretHall();
-        ~BarretHall();
-		void sleep();
-		           
-        void enter();
-        void exit();
-        void search(); 
-};
-
-
-class SchoolSupplies
-{
-	private:
-		bool isOwned;
-    public:
-        SchoolSupplies();
-        ~SchoolSupplies();
-		virtual void pickUp() = 0;
-		virtual void examine() = 0;
-		
-};
-
-class TextBook : public SchoolSupplies 
-{
-	private:
-		string title, text, author;
-    public:
-        TextBook();
-        ~TextBook();
-		void pickUp();
-		void examine();
-		void open();
-		void close();
-		
-		
-};
-
-class ID : public SchoolSupplies
-{
-	private:
-		string name;
-		int banner;
-    public:
-        ID();
-        ~ID();
-		void pickUp();
-		void examine();
-};
-
-class Laptop : public SchoolSupplies
-{
-	private:
-		int batteryPercentage;
-		string OS;
-    public:
-        Laptop();
-        ~Laptop();
-
-		void pickUp();
-		void examine();
-		void isDead();
-		void browseWeb();
-		void charge();
-};
-
-class Backpack :public SchoolSupplies
-{
-	private:
-		int size;
-		string whatItems[100];
-    public:
-        Backpack();
-        ~Backpack();
-		void pickUp();
-		void examine();
-		void isEmpty();
-		void get();
-		void place();
-		void contains();
-};
-
-class Charger : public SchoolSupplies
-{
-	private:
-		bool isPluggedIn;
-    public:
-        Charger();
-        ~Charger();
-		void pickUp();
-		void examine();
-};
-
+//class Moody : public Building
+//{
+//    private:
+//
+//    public:
+//        Moody();
+//        ~Moody();      
+//        void worship();
+//        
+//        void enter();
+//        void exit();
+//        void search(); 
+//};
+//
+//class RecCenter : public Building
+//{
+//    private:
+//
+//    public:
+//        RecCenter();
+//        ~RecCenter();
+//        void workOut();
+//                   
+//        void enter();
+//        void exit();
+//        void search(); 
+//};
+//class CampusCenter : public Building
+//{
+//	private:
+//		//
+//    public:
+//        CampusCenter();
+//        ~CampusCenter();
+//		void eatAtBean();
+//		           
+//        void enter();
+//        void exit();
+//        void search(); 
+//};
+//
+//class MBB : public Building
+//{
+//	private:
+//		//
+//    public:
+//        MBB();
+//        ~MBB();
+//		void goToClass();
+//		void isOnTime();
+//		           
+//        void enter();
+//        void exit();
+//        void search(); 
+//};
+//
+//class BarretHall : public Building
+//{
+//	private:
+//		//
+//    public:
+//        BarretHall();
+//        ~BarretHall();
+//		void sleep();
+//		           
+//        void enter();
+//        void exit();
+//        void search(); 
+//};
+//
+//
+//class SchoolSupplies
+//{
+//	private:
+//		bool isOwned;
+//    public:
+//        SchoolSupplies();
+//        ~SchoolSupplies();
+//		virtual void pickUp() = 0;
+//		virtual void examine() = 0;
+//		
+//};
+//
+//class TextBook : public SchoolSupplies 
+//{
+//	private:
+//		string title, text, author;
+//    public:
+//        TextBook();
+//        ~TextBook();
+//		void pickUp();
+//		void examine();
+//		void open();
+//		void close();
+//		
+//		
+//};
+//
+//class ID : public SchoolSupplies
+//{
+//	private:
+//		string name;
+//		int banner;
+//    public:
+//        ID();
+//        ~ID();
+//		void pickUp();
+//		void examine();
+//};
+//
+//class Laptop : public SchoolSupplies
+//{
+//	private:
+//		int batteryPercentage;
+//		string OS;
+//    public:
+//        Laptop();
+//        ~Laptop();
+//
+//		void pickUp();
+//		void examine();
+//		void isDead();
+//		void browseWeb();
+//		void charge();
+//};
+//
+//class Backpack :public SchoolSupplies
+//{
+//	private:
+//		int size;
+//		string whatItems[100];
+//    public:
+//        Backpack();
+//        ~Backpack();
+//		void pickUp();
+//		void examine();
+//		void isEmpty();
+//		void get();
+//		void place();
+//		void contains();
+//};
+//
+//class Charger : public SchoolSupplies
+//{
+//	private:
+//		bool isPluggedIn;
+//    public:
+//        Charger();
+//        ~Charger();
+//		void pickUp();
+//		void examine();
+//};
+//
 
--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 	// call player constructor
-	World world1;
+	World();
 	// call world constructor 
-	Player player1;
+	Player();
 	
 	// initial prompt to player about the game
 	cout << "WAKE UP!!! You've got a test in OOP today. \n You stayed up all night studying and you've left the things you need in various buuildings across campus that you visited last night. \n";
@@ -15,26 +15,24 @@ int main()
 
 	string userInput = "";
 	cin >> userInput; 
-	
-	while (gamecomplete = false)
+	bool tryWorked = false;
+	while (tryWorked == false)
 	{
 		try {
 		// test inputs
-		player1.move(userInput);	
+			tryWorked = true; 
+			Player::move(userInput);	
 		}
 		catch(Player::invalidMove){
+			
 			cout << "It's not a good idea to go there, you didn't leave any supplies for class there." << endl;
 			cin >> userInput;
 		}
 	}
-	
-	
-	// exceptions for any building we haven't included
-	//		"That's not a good idea, you didn't leave any supplies for class there" 
-
 
 	// once at a building player input could call the enter function
 	
 	// once inside they could call the exit or search or a specific member function 
-	 
+	 	// call player destructor
+
 }
