1e654ae8bb5795ad3b6a4e5a675e865b91c4b0b8
Gideon Luck <gbl12a@acu.edu>
2017-11-13 23:23:33 -0600

    Add files via upload

--- adventure.cpp
+++ adventure.cpp
@@ -1,3 +1,126 @@
 #include "adventure.h"
 
 
+World::World()
+{
+	exists = true;
+	Library();
+//	Moody();
+//	RecCenter();
+//	CampusCenter();
+//	MBB();
+//	BarretHall();
+}
+World::~World()
+{
+
+}
+void Player::move(string userinput) 
+{
+	string places[6] = {"Library", "Moody", "RecCenter", "CampusCenter", "MBB", "BarretHall"}; 
+	bool excepted = true; 
+	
+	for(int i = 0;i < 6; i++)
+	{
+		if(userinput == places[i] )
+		{
+			Library().enter();
+			excepted = false; 			
+		}
+
+	if(excepted  == true )
+		throw invalidMove();
+	}
+}
+Library::Library()
+{
+    location = 1;
+    hasBeenVisited = false;
+    isLocked = false;
+
+}
+Library::~Library()
+{
+	isLocked = false;
+}
+void Library::getCoffee()
+{
+	cout << "You stopped by the Starbucks and spent $5.60 on a cup of bad coffee." << endl;
+	cout << "You don't have a lot of time left to look around." << endl;
+	string userInput;
+	cin >> userInput;
+	bool tryWorked = false;
+	while (tryWorked == false)
+	{
+		try{
+			tryWorked = true;
+			Library::doThis(userInput);
+		}
+		catch(Library::tryagain){
+			cout << "That's not a valid command. For a list of valid commands type 'help'." << endl;
+			Library::doThis(userInput);
+		}
+	}
+}
+void Library::enter()
+{
+	Library::hasBeenVisited = true; 
+	string userInput;
+	cout << "You have entered the library, you think you have left your textbook here. You may search for items by entering in 'Search'. You may leave by entering 'Exit'." << endl;
+	cin >> userInput;
+	bool tryWorked = false;
+	while (tryWorked == false)
+	{
+		try{
+			tryWorked = true;
+			Library::doThis(userInput);
+		}
+		catch(Library::tryagain){
+			cout << "That's not a valid command. For a list of valid commands type 'help'." << endl;
+			Library::doThis(userInput);
+		}
+	}
+		
+}
+void Library::exit()
+{
+	string userInput;
+	cout << "You've exited the Library. What building do you want to look in next?" << endl;
+	cin >> userInput;
+	
+	bool tryWorked = false;
+	while (tryWorked == false)
+	{
+		try {
+			// test inputs
+			tryWorked = true;
+			Player::move(userInput);	
+		}
+		catch(Player::invalidMove){
+			
+			cout << "It's not a good idea to go there, you didn't leave any supplies for class there." << endl;
+			
+		}
+	}
+}
+void Library::search()
+{
+	string userInput;
+	cout << "You've found your textbook! " << endl << "You're one step closer to getting to that test!" << endl;
+	cout << "You should keep looking for the rest of your supplies." << endl;
+	
+	Library::exit();
+
+}
+void Library::doThis(string userInput)
+{
+	if(userInput == "Exit")
+		Library::exit();
+	else if (userInput == "Search")
+		Library::search();
+	else if(userInput == "getCoffee")
+		Library::getCoffee();
+	else
+		throw tryagain();
+
+}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
 #include "adventure.cpp"
 
-using namespace std;
-
-
 int main()
 {
 	// call player constructor
+	World();
 	// call world constructor 
+	Player();
 	
 	// initial prompt to player about the game
-	
+	cout << "WAKE UP!!! You've got a test in OOP today. \n You stayed up all night studying and you've left the things you need in various buuildings across campus that you visited last night. \n";
 	// give the user options (which building do you want to try to  go to)
 	//	type a building name or help to see ooptions 
 	
-	
-	// exceptions for any building we haven't included
-	//		"That's not a good idea, you didn't leave any supplies for class there" 
-	
+
+	string userInput = "";
+	cin >> userInput; 
+	bool tryWorked = false;
+	while (tryWorked == false)
+	{
+		try {
+		// test inputs
+			tryWorked = true; 
+			Player::move(userInput);	
+		}
+		catch(Player::invalidMove){
+			
+			cout << "It's not a good idea to go there, you didn't leave any supplies for class there." << endl;
+			cin >> userInput;
+		}
+	}
+
 	// once at a building player input could call the enter function
 	
 	// once inside they could call the exit or search or a specific member function 
-	
-	 
+	 	// call player destructor
+
 }
